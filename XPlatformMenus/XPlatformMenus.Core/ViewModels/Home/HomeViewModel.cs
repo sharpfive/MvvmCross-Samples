078138e705720a8c7475c30317187c0e04ce0974
Jaim Zuber <Jaim.Zuber@spok.com>
2018-01-12 11:39:28 -0600

    Add Navigation Service to HomeViewModel

--- XPlatformMenus/XPlatformMenus.Core/ViewModels/Home/HomeViewModel.cs
+++ XPlatformMenus/XPlatformMenus.Core/ViewModels/Home/HomeViewModel.cs
@@ -1,12 +1,17 @@
-﻿using MvvmCross.Core.ViewModels;
+﻿using System;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
 
 namespace XPlatformMenus.Core.ViewModels
 {
 	public class HomeViewModel : BaseViewModel
     {
-        public HomeViewModel() 
+        private readonly IMvxNavigationService _navigationService;
+
+        public HomeViewModel(IMvxNavigationService navigationService) 
         {
             Recycler = new RecyclerViewModel();
+            _navigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
         }
 
         /// <summary>Gets the recycler.</summary>
